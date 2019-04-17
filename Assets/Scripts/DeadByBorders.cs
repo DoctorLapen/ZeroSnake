528ad97de72590d27e923b26ece6f6477c0fdba1
DoctorLapen <kostia1272@gmail.com>
2019-04-17 22:54:58 +0300

    aad deead by borders and reload after dead

---
+++ Assets/Scripts/DeadByBorders.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.SceneManagement;
+using UnityEngine;
+
+public class DeadByBorders : MonoBehaviour
+{
+    // Start is called before the first frame update
+    private void OnTriggerEnter(Collider snake)
+    {
+        if (snake.CompareTag("snakeHead") || snake.CompareTag("snake"))
+        {
+            SceneManager.LoadScene(SceneManager.GetActiveScene().name);
+        }
+
+    }
+}
